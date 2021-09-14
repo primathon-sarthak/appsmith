bbb85950ccfb1bab1f4acfc8dc099b6a8fa64ca0
sbalaji1192 <sbalaji1192@gmail.com>
2021-09-14 13:13:48 +0530

    wip

--- app/client/src/constants/ThirdPartyConstants.tsx
+++ app/client/src/constants/ThirdPartyConstants.tsx
@@ -3,3 +3,13 @@ export const S3_BUCKET_URL =
   "https://s3.us-east-2.amazonaws.com/assets.appsmith.com";
 export const TELEMETRY_URL = "https://docs.appsmith.com/telemetry";
 export const DISCORD_URL = "https://discord.gg/rBTTVJp";
+export const GITHUB_RELEASE_URL =
+  "https://github.com/appsmithorg/appsmith/releases/tag";
+export const GET_RELEASE_NOTES_URL = (tagName: string) =>
+  `${GITHUB_RELEASE_URL}/${tagName}`;
+export const GOOGLE_MAPS_SETUP_DOC =
+  "https://docs.appsmith.com/setup/docker/google-maps";
+export const GOOGLE_SIGNUP_SETUP_DOC =
+  "https://docs.appsmith.com/setup/docker/google-login";
+export const GITHUB_SIGNUP_SETUP_DOC =
+  "https://docs.appsmith.com/setup/docker/github-login";
